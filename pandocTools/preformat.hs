3f775176418efb27aa2a561604d31c1957c742a2
Harald Geyer <harald@lefant.net>
2011-03-17 00:49:01 +0100

    Implement pattern matching for preformat

--- pandocTools/preformat.hs
+++ pandocTools/preformat.hs
@@ -4,6 +4,9 @@
 -- cleanup HTML to make it more pandoc friendly
 
 import Data.List
+import Data.List.Split
+import Data.Maybe
+import Control.Monad
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Tree
 import System.IO
@@ -32,6 +35,8 @@ parseSetup ("report1":file:_) s = s {report1 = Just file}
 parseSetup ("report2":file:_) s = s {report2 = Just file }
 parseSetup ("ignore":tag) s = s {rules = (ignore tag : rules s)}
 parseSetup ("remove":tag) s = s {rules = (remove tag : rules s)}
+parseSetup ("replace":sel:templ:_) s = s {rules = (replace sel templ : rules s)}
+parseSetup ("killattr":sel:attr) s = s {rules = (killattr sel attr : rules s)}
 parseSetup line _ = error ("Can't parse: " ++ (unwords line))
 
 ignore :: [String] -> TagTree String -> [TagTree String]
@@ -43,6 +48,55 @@ remove :: [String] -> TagTree String -> [TagTree String]
 remove s t = if match t s
 	then []
 	else [t]
+
+killattr :: String -> [String] -> TagTree String -> [TagTree String]
+killattr selector as t = if selects (splitOn ";" selector) t
+	then [filterAttrs t]
+	else [t]
+	where 
+		filterAttrs (TagBranch name attrs c) = 
+			(TagBranch name (filter pred attrs) c)
+		filterAttrs (TagLeaf (TagOpen name attrs)) =
+			(TagLeaf (TagOpen name (filter pred attrs)))
+		filterAttrs t = t
+		pred x = not (elem (fst x) as)
+
+replace :: String -> String -> TagTree String -> [TagTree String]
+replace selector template t = if selects (splitOn ";" selector) t
+	then [write (map attrPair (splitOn ";" template)) t]
+	else [t]
+	where 
+		write (("tag", n):xs) (TagLeaf (TagOpen _ as)) = 
+			(TagLeaf (TagOpen n (h as xs)))
+		write xs (TagLeaf (TagOpen n as)) = 
+			(TagLeaf (TagOpen n (h as xs)))
+		write (("tag", n):xs) (TagBranch _ as c) = 
+			(TagBranch n (h as xs) c)
+		write xs (TagBranch n as c)          = (TagBranch n (h as xs) c)
+		h as = mapMaybe (h2 as)
+		h2 as (i,[]) = liftM (\s -> (i,s)) $ lookup i as
+		h2 _  p      = Just p
+			
+
+selects :: [String] -> TagTree String -> Bool
+selects xs t = all (matches t) (map attrPair xs)
+	where 
+		matches (TagBranch name as _) p@(e1, e2) 
+			| e1 == "tag" = e2 == name
+			| e2 == []    = elem e1 $ map fst as
+			| otherwise   = elem p as
+		matches (TagLeaf (TagOpen name as)) p@(e1, e2)
+                        | e1 == "tag" = e2 == name
+                        | e2 == []    = elem e1 $ map fst as
+                        | otherwise   = elem p as
+		matches _ _ = False
+
+attrPair :: String -> (String, String)
+attrPair = h . (break (== '='))
+	where
+		h (e1,('=':e2)) = (e1, e2)
+		h p = p
+
 
 tagContent :: TagTree String -> [TagTree String]
 tagContent (TagBranch _ _ ts) = ts
